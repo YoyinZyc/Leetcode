209a110e9d9915a14e44a4587931cd7d9554eaf9
Cactus <Cactus@192.168.1.103>
2017-09-02 12:43:47 -0400

    dp 9.2

--- LeetCode/src/main/Pro64_Minimum_Path_Sum.java
+++ LeetCode/src/main/Pro64_Minimum_Path_Sum.java
@@ -6,5 +6,25 @@ package main;
  * 
  */
 public class Pro64_Minimum_Path_Sum {
-
+	public int minPathSum(int[][] grid) {
+		if(grid.length == 0) return 0;
+        int[][] steps = new int[grid.length][grid[0].length];
+		
+		steps[0][0] = grid[0][0];
+		for (int i = 1; i < steps.length; i++) {
+			steps[i][0] = steps[i-1][0] + grid[i][0];
+		}
+		
+		for (int i = 1; i < steps[0].length; i++) {
+			steps[0][i] = steps[0][i-1] + grid[0][i];
+		}
+		
+		
+		for (int i = 1; i < steps.length; i++) {
+			for (int j = 1; j < steps[i].length; j++) {
+				steps[i][j] = grid[i][j] + Math.min(steps[i-1][j], steps[i][j-1]);
+			}
+		}
+		return steps[grid.length-1][grid[0].length-1];
+	}
 }
