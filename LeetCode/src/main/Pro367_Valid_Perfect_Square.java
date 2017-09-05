c5511303065cf016fc99b8c2a93f5b8ed260ef63
YoyinZyc <yoyinzyc@gmail.com>
2017-09-05 00:38:21 -0400

    9.4 BS Easy 1

---
+++ LeetCode/src/main/Pro367_Valid_Perfect_Square.java
@@ -0,0 +1,29 @@
+package main;
+
+import javax.xml.stream.events.StartDocument;
+
+/**
+ * @author Cactus
+ * @category BS_Easy
+ * @version Create at: Sep 5, 2017 12:13:00 AM
+ */
+public class Pro367_Valid_Perfect_Square {
+	public boolean isPerfectSquare(int num) {
+		if(num == 1) return true;
+		if(num > 1 && num < 4) return false;
+		if(num == 4) return true;
+		int start = 2;
+		int end = (num+1)/2;
+		while(end-start > 1) {
+			int index = (end-start)/2;
+			if((long)((end-index) * (end-index)) == num) return true;
+			if ((end-index) * (end-index) > num) {
+				end = end-index;
+			}else {
+				start = end-index;
+			}
+		}
+		return false;
+		
+	}
+}
