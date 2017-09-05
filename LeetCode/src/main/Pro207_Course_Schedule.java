086d3036b765e10ad26f20230e1f429be398313a
YoyinZyc <yoyinzyc@gmail.com>
2017-09-04 23:28:56 -0400

    9.4 DSP Medium 1

--- LeetCode/src/main/Pro207_Course_Schedule.java
+++ LeetCode/src/main/Pro207_Course_Schedule.java
@@ -9,27 +9,35 @@ public class Pro207_Course_Schedule {
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
 		int i = 0;
 		int[][] preGraph = new int[numCourses][numCourses];
-		for (int j = 0; j < preGraph.length; j++) {
+		for (int j = 0; j < prerequisites.length; j++) {
 			preGraph[prerequisites[j][0]][prerequisites[j][1]] = 1;
 		}
-		int[] record = new int[numCourses];
+		int[] trueRecord = new int[numCourses];
+		
 		while(i < numCourses) {
-			if(record[i] == 0 ) {
-				if(!dfs(i, preGraph,record)) return false; 
+			if(trueRecord[i] == 0 ) {
+				int[] record = new int[numCourses];
+				if(!dfs(i, preGraph,record, trueRecord)) return false; 
 			}
 			i++;
 		}
 		return true;
 	}
 	
-	private boolean dfs(int i, int[][] preGraph, int[] record) {
+	private boolean dfs(int i, int[][] preGraph, int[] record, int[] trueRecord) {
 		record[i] = 1;
 		for (int j = 0; j < preGraph[i].length; j++) {
 			if(preGraph[i][j] == 1) {
-				if(record[j] == 0) {
-					dfs(j, preGraph, record);
-				}else {
-					return false;
+				if (trueRecord[j] == 0) {
+					if (record[j] == 0) {
+						if(dfs(j, preGraph, record, trueRecord)) {
+							trueRecord[j] = 1;
+						}else {
+							return false;
+						}
+					}else {
+						return false;
+					}
 				}
 			}
 		}
